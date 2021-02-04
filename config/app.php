12d76f9c9ad617ac6a8d5f0946f97d464bf924bb
flashscript <rubensaircortez@gmail.com>
2021-02-04 17:03:33 -0400

    build: add subscriptions with pusher service

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         \Nuwave\Lighthouse\Subscriptions\SubscriptionServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        \Nuwave\Lighthouse\Subscriptions\SubscriptionServiceProvider::class,
 
     ],
 
